1ac6bf3573febc69d2398c4a23c8775a470dd709
Emil Skogsund <emil.skogsund@gmail.com>
2024-07-14 14:31:50 +0200

    feature/disable

---
+++ lua/core/plugins/disable.lua
@@ -0,0 +1,10 @@
+return {
+  {"folke/flash.nvim", enabled = false},
+  {"nvimdev/dashboard-nvim", enabled = false},
+  {"echasnovski/mini.ai", enabled = false},
+  {"echasnovski/mini.icons", enabled = false},
+  {"echasnovski/mini.pairs", enabled = false},
+  {"folke/noice.nvim", enabled = false},
+  {"windwp/nvim-autopairs", enabled = false},
+  {"roobert/tailwindcss-colorizer-cmp.nvim", enabled = false},
+}
