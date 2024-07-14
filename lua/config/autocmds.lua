7fa84b87563201eb64aae5ebb96e16308ff10621
Emil Skogsund <emil.skogsund@gmail.com>
2024-07-14 09:28:25 +0200

    feat(autocmds): Add last location jump on buffer open
    This commit introduces a new autocmd for BufReadPost event. The purpose
    of this autocmd is to return the cursor to the last location when a
    buffer is opened. This feature is useful for navigating large files and
    improves user experience. The autocmd excludes certain filetypes like
    'gitcommit', 'commit', 'gitrebase' and buffers that have already
    triggered the autocmd.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -23,3 +23,21 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank({ timeout = 50 })
 	end,
 })
+
+-- Go to last loc when opening a buffer, see ':h last-position-jump'
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup("last_loc"),
+  callback = function(event)
+    local exclude = { "gitcommit", "commit", "gitrebase" }
+    local buf = event.buf
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].lazyvim_last_loc then
+      return
+    end
+    vim.b[buf].lazyvim_last_loc = true
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(buf)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
